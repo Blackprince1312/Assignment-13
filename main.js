c8031b254314d3ae667a3810f2fc2e8f4d2c6dcf
Elijah Prince <elijahprince73@gmail.com>
2017-09-03 17:03:50 -0700

    added options to select

--- main.js
+++ main.js
@@ -4,34 +4,28 @@ $(document).ready(function() {
     method: 'GET',
     dataType: 'json',
     success: function(result) {
+
       result.forEach(function(element) {
 
-        console.log(element);
+        document.getElementById('formTemplate').innerHTML += `
+          <input type="${element.type}" value="${element.label}"/>`
 
         var options = element.options
         var select = function(options) {
           for (var i = 0; i < options.length; i++) {
             var optionsSelect = options[i].label
-            var option = document.createElement("option");
-            option.text = optionsSelect;
-            option.value = "myvalue";
-            var select = document.getElementById("formTemplate");
-            select.appendChild(option);
-
+            var node = document.createElement('option')
+            var textnode = document.createTextNode(optionsSelect)
+            node.appendChild(textnode)
+            document.getElementById('formTemplate').innerHTML += `<select id="options"></select>`
+            document.getElementById('options').appendChild(node)
           }
         }
 
-        select(options);
-        document.getElementById('formTemplate').innerHTML += `
-          <input type="${element.type}" value="${element.label}"/>
-          `
+        select(options)
+
       })
+
     }
   })
 })
-
-// var result = jsObjects.filter(function(obj) {
-//   return obj.b == 6;
-// });
-//
-// console.log(result);
